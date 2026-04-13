e40b592ab94952be22d870f763313ae39eb98c14
MachineLearning <rzadanowsky@albany.edu>
2026-04-13 19:55:18 -0400

    Finalized Assignment 5

--- OS/src/Pages4.java
+++ OS/src/Pages4.java
@@ -2,17 +2,17 @@
 // allocates a space in memory which returns a pointer to the starting address
 // in the allocated block and then proceeds to write data into memory
 // and reads the data back from memory. It tests read/write function of Hardware
-// and also test memory allocation. This now implements a test where Page4 process writes
-// in memory, then sleeps while Page5 process also writes in memory, then Page4 proceeds to
+// and also test memory allocation. This now implements a test where Pages4 process writes
+// in memory, then sleeps while Pages5 process also writes in memory, then Pages4 proceeds to
 // read data back from memory where it checks if it's the same data it initially wrote
-// to memory, where it wasn't overwritten by Page5.
+// to memory, where it wasn't overwritten by Pages5.
 public class Pages4 extends UserlandProcess{
 
     @Override
     /**
      * This function tests the memory allocation functionality as well as read/write
      * functionality of the Hardware class. This is the same as Pages1 but now
-     * implements a test to ensure that Page4 process wasn't overwritten by Page5 process,
+     * implements a test to ensure that Pages4 process wasn't overwritten by Pages5 process,
      * an isolation test.
      */
     public void main() {
--- OS/src/Pages5.java
+++ OS/src/Pages5.java
@@ -2,11 +2,11 @@
 // allocates a space in memory which returns a pointer to the starting address
 // in the allocated block and then proceeds to write data into memory
 // and reads the data back from memory. It tests read/write function of Hardware
-// and also test memory allocation. This now implements a test where Page4 process writes
-// in memory, then sleeps while Page5 process also writes in memory, then Page4 proceeds to
+// and also test memory allocation. This now implements a test where Pages4 process writes
+// in memory, then sleeps while Pages5 process also writes in memory, then Pages4 proceeds to
 // read data back from memory where it checks if it's the same data it initially wrote
-// to memory, where it wasn't overwritten by Page5. Page5 also checks if the data it wrote initially is the
-// same data it read back. Now test FreeMemory as well to ensure the assigned memory
+// to memory, where it wasn't overwritten by Pages5. Pages5 also checks if the data it wrote initially is the
+// same data it reads back. Now test FreeMemory as well to ensure the assigned memory
 // space to the process is freed and is no longer reachable.
 public class Pages5 extends UserlandProcess{
 
@@ -14,8 +14,8 @@ public class Pages5 extends UserlandProcess{
     /**
      * This function tests the memory allocation functionality as well as read/write
      * functionality of the Hardware class. This is the same as Pages1 but now
-     * implements a test to ensure that Page4 process wasn't overwritten by Page5 process,
-     * and Page5 process wasn't overwritten by Page4 process, an isolation test. It also implements a
+     * implements a test to ensure that Pages4 process wasn't overwritten by Pages5 process,
+     * and Pages5 process wasn't overwritten by Pages4 process, an isolation test. It also implements a
      * test for FreeMemory.
      */
     public void main() {
